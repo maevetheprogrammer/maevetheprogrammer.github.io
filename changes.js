3fc373503d98b2eabe40bf60cae3220c8ec2fc29
maevetheprogrammer <53616341+maevetheprogrammer@users.noreply.github.com>
2019-08-02 11:30:12 -0400

    Initial commit

---
+++ changes.js
@@ -0,0 +1,5 @@
+function change_background() {
+  //document.getElementById('change_background').style.backgroundColor = "Red";
+  document.body.style.backgroundColor = "Green";
+  document.body.style.color = "Red"
+}
